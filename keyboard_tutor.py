2c54cde3932a5cbf28f8b7e7a223a42718abf252
Darya <dashav1605@mail.ru>
2021-10-08 13:18:06 +0500

    Add text topics, update checking and marking rules

--- keyboard_tutor.py
+++ keyboard_tutor.py
@@ -15,20 +15,31 @@ VALID_SYMBOLS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
                  'G', 'H', 'J', 'K', 'L', 'Y', 'X', 'C', 'V', 'B',
                  'N', 'M', '^', '!', '"', '$', '%', '&', "/", '(',
                  ')', '=', '?', ' ', '+', '<', ',', '.', '-', '_',
-                 '*', '#', "'", ':', ';', '>', '@', '{', '[', ']', '}']
+                 '*', '#', "'", ':', ';', '>', '@', '{', '[', ']',
+                 '}']
+
+FIRST_MESSAGE = "Let's typing! " \
+                "Choose training topic at the top of the window.\n\n" \
+                "If you are new, you can warm up with easy and fast topic " \
+                "\"Letters\", \"Numbers\" or \"Punctuation\".\n\n" \
+                "If you want something more serious and hard, you can choose " \
+                "\"Words\", \"Quotations\" or \"Long Text\".\n\n" \
+                "If you want something special, you can choose \"Python\" or \"TypeScript\" " \
+                "to practice fast typing some of key words of this programming languages."
 
 
 class KeyboardTutor(tkinter.Text):
-    def __init__(self, frame, symbols_counter, current_speed_counter):
+    def __init__(self, frame, symbols_counter, current_speed_counter, text_topic, frame_for_focus):
 
         scrollbar = tkinter.Scrollbar(frame, orient=tkinter.VERTICAL)
         scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
-        super().__init__(frame, wrap=tkinter.WORD, bg="white", height=20, width=75, font=('Arial', 15),
-                         yscrollcommand=scrollbar.set, padx=5, pady=10, borderwidth=5)
+        super().__init__(frame, wrap=tkinter.WORD, bg="white", height=20,
+                         width=75, font=('Times', 17), yscrollcommand=scrollbar.set,
+                         padx=5, pady=10, borderwidth=5)
         scrollbar.config(command=self.yview)
         self.pack(expand=True, fill="both")
 
-        self.tag_config("correct", background="white smoke", foreground="green")
+        self.tag_config("correct", background="white smoke", foreground="#465945")
         self.tag_config("incorrect", background="misty rose", foreground="red")
         self.tag_config("current_position", background="#FFDB58")
 
@@ -45,20 +56,79 @@ class KeyboardTutor(tkinter.Text):
         self.symbols_counter = symbols_counter
         self.current_speed_counter = current_speed_counter
         self.mistakes_count = 0
+        self.frame_for_focus = frame_for_focus
 
-        self.texts = []
+        self.texts_json = None
         self._load_texts()
+        self.text_topic = text_topic
+        self.text_topic.bind("<<ComboboxSelected>>", self.reload_text)
         self.show()
 
+    def _load_quotations(self):
+        self.text = TextCleaner.clean_text(
+            random.choice(self.texts_json["Quotations"]))
+
+    def _load_long_text(self):
+        self.text = random.choice(self.texts_json["Long Text"])
+
+    def _load_numbers(self):
+        numbers_count = random.randint(50, 71)
+        text = f'{random.randint(0, 1000)}'
+        for _ in range(numbers_count):
+            text += f' {random.randint(0, 1000000)}'
+        self.text = text
+
+    def _load_symbols(self, topic):
+        count = random.randint(50, 71)
+        text = f'{random.choice(self.texts_json[topic])}'
+        for _ in range(count):
+            text += f' {random.choice(self.texts_json[topic])}'
+        self.text = text
+
+    def _load_words(self):
+        words_count = random.randint(20, 41)
+        text = f'{random.choice(self.texts_json["Words"])}'
+        for _ in range(words_count):
+            if random.choice([True, False]):
+                text += f'\n{random.choice(self.texts_json["Words"])}'
+            else:
+                text += f'\n{random.choice(self.texts_json["Words"]).title()}'
+        self.text = text
+
+    def _load_programming_languages(self, topic):
+        words_count = random.randint(20, 41)
+        text = f'{random.choice(self.texts_json[topic])}'
+        for _ in range(words_count):
+            text += f'\n{random.choice(self.texts_json[topic])}'
+        self.text = text
+
+    def _load_text(self):
+        topic = self.text_topic.get()
+        if topic == "Quotations":
+            self._load_quotations()
+        elif topic == "Long Text":
+            self._load_long_text()
+        elif topic == "Numbers":
+            self._load_numbers()
+        elif topic == "Letters" or topic == "Punctuation":
+            self._load_symbols(topic)
+        elif topic == "Words":
+            self._load_words()
+        else:
+            self._load_programming_languages(topic)
+
     def do_first_step(self):
-        self.insert(tkinter.END, "Let's typing!\n\nPress any key to start.\n")
+        if self.text_topic.get() == "Choose training topic":
+            self.insert(tkinter.END, FIRST_MESSAGE)
+        else:
+            self.insert(tkinter.END, "Text loaded!\n\nPress any key to start.\n")
         self.symbols_count = 0
         self.symbols_counter.set(0)
         self.current_speed_counter.set(0)
         self.mistakes_count = 0
-        self.text = random.choice(self.texts)
 
     def do_training_step(self):
+        self._load_text()
         self.symbols_count = len(self.text)
         self.symbols_counter.set(self.symbols_count)
         self.printed_symbols_count = 0
@@ -102,8 +172,7 @@ class KeyboardTutor(tkinter.Text):
 
     def _load_texts(self):
         with open("texts.json") as file:
-            texts_json = json.load(file)
-            self.texts = [TextCleaner.clean_text(txt) for txt in texts_json.values()]
+            self.texts_json = json.load(file)
 
     @staticmethod
     def check_and_get_char(event):
@@ -121,15 +190,18 @@ class KeyboardTutor(tkinter.Text):
             self.current_step = 0
         self.show()
 
-    def reload_text(self):
+    def reload_text_with_button(self):
         self.current_step = 0
         self.show()
 
-    def is_corrected_mistake(self, move_correct, backspace):
-        return move_correct and not backspace and self.index('current_position_mark') in self.incorrect
+    def reload_text(self, event):
+        self.current_step = 0
+        self.frame_for_focus.focus()
+        self.show()
 
     def is_made_mistake(self, move_correct):
-        return not move_correct or self.compare('current_position_mark', '!=', 'correct_mark')
+        return not move_correct \
+               or self.compare('current_position_mark', '!=', 'correct_mark')
 
     def type(self, event):
         if self.steps[self.current_step] == 'Training':
@@ -139,7 +211,8 @@ class KeyboardTutor(tkinter.Text):
             current_char = self.get(self.tag_ranges('current_position')[0],
                                     self.tag_ranges('current_position')[1])
             if key:
-                move_correct = (key == current_char or (key == "Return" and current_char == '\u23CE'))
+                move_correct = (key == current_char or (key == "Return"
+                                                        and current_char == '\u23CE'))
                 if move_correct:
                     self.printed_symbols_count += 1
 
@@ -150,10 +223,18 @@ class KeyboardTutor(tkinter.Text):
                 self.update_marks(move_correct)
                 self.add_tags()
 
+                self.scroll()
+
             if self.is_end_of_text():
                 self.change_step()
-        else:
+        elif self.text_topic.get() != "Choose training topic":
             self.change_step()
+
+    def scroll(self):
+        current_y = self.dlineinfo('current_position_mark')[1]
+        height = self.winfo_height()
+        if current_y + 80 > height:
+            self.yview_scroll(height // 2, 'pages')
 
     def update_marks(self, move_correct=True):
         if move_correct:
--- main_window.py
+++ main_window.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 """Главное окно приложения"""
 
-
 ERROR_PYTHON_VERSION = 1
 
 import sys
-
 
 if sys.version_info < (3, 6):
     print('Use python >= 3.6', file=sys.stderr)
@@ -15,22 +13,19 @@ __version__ = '1.1'
 __author__ = 'Vikhlyantseva Darya'
 __email__ = 'dashav1605@gmail.com'
 
-
 import tkinter
+from tkinter import ttk
 from keyboard_tutor import KeyboardTutor
-
-
-# class TextChooser:
-#     def __init__(self, frame):
-
 
 
 class SpeedLabel:
     def __init__(self, frame):
         self.counter = tkinter.StringVar()
         self.counter.set(0)
-        speed_text = tkinter.Label(frame, text="Current speed (sym / min): ", font=('Arial', 12, 'bold'))
-        speed_counter = tkinter.Label(frame, textvariable=self.counter, font=('Arial', 12), anchor="w")
+        speed_text = tkinter.Label(frame, text="Current speed (sym / min): ",
+                                   font=('Arial', 12, 'bold'))
+        speed_counter = tkinter.Label(frame, textvariable=self.counter,
+                                      font=('Arial', 12), anchor="e", width=5)
         speed_counter.pack(side=tkinter.RIGHT, padx=10)
         speed_text.pack(side=tkinter.RIGHT)
 
@@ -39,9 +34,11 @@ class SymbolsCounterLabel:
     def __init__(self, frame):
         self.counter = tkinter.StringVar()
         self.counter.set(0)
-        symbols_count_text = tkinter.Label(frame, text="Symbols:", font=('Arial', 12, 'bold'))
+        symbols_count_text = tkinter.Label(frame, text="Symbols:",
+                                           font=('Arial', 12, 'bold'))
         symbols_count_text.pack(side=tkinter.LEFT, padx=10)
-        symbols_count = tkinter.Label(frame, textvariable=self.counter, font=('Arial', 12), anchor="w")
+        symbols_count = tkinter.Label(frame, textvariable=self.counter,
+                                      font=('Arial', 12), anchor="w")
         symbols_count.pack(side=tkinter.LEFT, padx=5)
 
 
@@ -53,6 +50,8 @@ class MainWindow(tkinter.Tk):
         self.minsize(600, 300)
         self.geometry(f"+{self.winfo_screenwidth() // 5}+{self.winfo_screenheight() // 5}")
         self.resizable(width=False, height=False)
+        self.font = ('Arial', 12)
+        self.option_add('*TCombobox*Listbox.font', self.font)
 
         bottom_frame = tkinter.Frame(self)
         bottom_frame.pack(side=tkinter.BOTTOM, fill='x')
@@ -65,13 +64,22 @@ class MainWindow(tkinter.Tk):
 
         symbols_counter_label = SymbolsCounterLabel(top_frame)
         speed_label = SpeedLabel(top_frame)
-        keyboard_tutor = KeyboardTutor(center_frame, symbols_counter_label.counter, speed_label.counter)
 
-        quit_button = tkinter.Button(bottom_frame, text='Quit', font=('Arial', 12), command=self.quit)
+        topic = ttk.Combobox(top_frame, values=['Letters', 'Punctuation', 'Numbers', 'Words',
+                                                'Quotations', 'Long Text', 'Python', 'TypeScript'],
+                             font=self.font, state='readonly')
+        topic.set('Choose training topic')
+        topic.pack(side=tkinter.TOP)
+
+        keyboard_tutor = KeyboardTutor(center_frame, symbols_counter_label.counter,
+                                       speed_label.counter, topic, top_frame)
+
+        quit_button = tkinter.Button(bottom_frame, text='Quit',
+                                     font=('Arial', 12), command=self.quit)
         quit_button.pack(side=tkinter.RIGHT, padx=10, pady=5)
 
-        reload_button = tkinter.Button(bottom_frame, text='Another text', font=('Arial', 12),
-                                       command=keyboard_tutor.reload_text)
+        reload_button = tkinter.Button(bottom_frame, text='Reload text', font=('Arial', 12),
+                                       command=keyboard_tutor.reload_text_with_button)
         reload_button.pack(side=tkinter.LEFT, padx=10, pady=5)
 
 
